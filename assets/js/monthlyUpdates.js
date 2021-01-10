3f44235ba9841ac6517200610d5604b3cbf5e55e
Dhairya Ostwal <50984984+dhairyaostwal@users.noreply.github.com>
2021-01-10 22:12:19 +0530

    Update monthlyUpdates.js

--- assets/js/monthlyUpdates.js
+++ assets/js/monthlyUpdates.js
@@ -1,25 +1,26 @@
+import monthlyUpdates from "./updatesFromTech"
 var d = new Date();
 var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.getElementById("month").innerHTML = (monthName[d.getMonth()] + " Updates");
 
 
-var monthlyUpdates = [
-    {
-        "announcement": "We welcome the new recruits in SSIT! 🧑‍💻 👩‍💻"
-    },
-    {
-        "announcement": "Accomplishment 🎉: GitMash 🎮"
-    },
-    {
-        "announcement": "Accomplishment 🎉: Data Science project of COVID-19"
-    },
-    {
-        "announcement": "Ongoing 🏃‍♂️: App Dev Projects"
-    },
-    {
-        "announcement": "Ongoing 🏃‍♂️: Farmer's Marketplace"
-    }
-]
+// var monthlyUpdates = [
+//     {
+//         "announcement": "We welcome the new recruits in SSIT! 🧑‍💻 👩‍💻"
+//     },
+//     {
+//         "announcement": "Accomplishment 🎉: GitMash 🎮"
+//     },
+//     {
+//         "announcement": "Accomplishment 🎉: Data Science project of COVID-19"
+//     },
+//     {
+//         "announcement": "Ongoing 🏃‍♂️: App Dev Projects"
+//     },
+//     {
+//         "announcement": "Ongoing 🏃‍♂️: Farmer's Marketplace"
+//     }
+// ]
 
 const updates = document.getElementById('updates')
 var i;
